d4065a41e57e82ddb24e5adb9476541557a376c5
Pavel <pvlkozyr@outlook.com>
2019-05-19 11:26:05 +0300

    query implemented

--- Query.cpp
+++ Query.cpp
@@ -6,7 +6,9 @@
 #include "Record.h"
 
 std::string Query::read_field(std::string field_name, std::string query) {
-    return std::__cxx11::string();
+    unsigned int pos = query.find(field_name);
+    unsigned  int pos1 = query.find('\t',pos+1);
+    return query.substr(pos+1,pos1);
 }
 
 Query::Query(std::string query) {
@@ -62,6 +64,78 @@ Range::Range(std::string str_range) {
     }
 }
 
-Range::~Range() {
+bool IpRange::check(Ip ip) {
+    if (o1 == -1 && o2 == -1 && o3 == -1 && o4 == -1)
+        return true;
+    else if (o1 == ip.o1 && o2 == ip.o2 && o3 == ip.o3 && o4 == -1)
+        return true;
+    else if (o1 == ip.o1 && o2 == ip.o2 && o3 == -1 && o4 == -1)
+        return true;
+    else if (o1 == ip.o1 && o2 == -1 && o3 == -1 && o4 == -1 )
+        return true;
+    else if (o1 == ip.o1 && o2 == ip.o2 && o3 == ip.o3 && o4 == ip.o4)
+        return true;
+    else return false;
+}
 
+IpRange::IpRange(std::string str_range)
+{
+    o1 = -1;
+    o2 = -1;
+    o3 = -1;
+    o4 = -1;
+    if (str_range.size() == 0) return;
+    unsigned int pos = str_range.find('.');
+    o1 = std::stoi(str_range.substr(0,pos));
+    unsigned int pos1 = str_range.find('.',pos+1);
+    o2 = std::stoi(str_range.substr(pos,pos1));
+    pos = str_range.find('.',pos1+1);
+    o3 = std::stoi(str_range.substr(pos1,pos));
+    o4 = std::stoi(str_range.substr(pos+1));
+}
+
+
+bool DateRange::check(Date date)
+{
+    if (year == -1 && month == -1 && day == -1)
+        return true;
+    else if (year == date.year && month == date.month && day == -1)
+        return true;
+    else if (year == date.year && month == -1 && day == -1)
+        return true;
+    else if (year == date.year && month == date.month && day == date.day)
+        return true;
+    else return false;
+}
+
+DateRange::DateRange(std::string str_range)
+{
+    year = -1;
+    month = -1;
+    day = -1;
+    if (str_range.size() == 0) return;
+    unsigned int pos = str_range.find('.');
+    year = std::stoi(str_range.substr(0,pos));
+    unsigned int pos1 = str_range.find('.',pos+1);
+    month = std::stoi(str_range.substr(pos,pos1));
+    day = std::stoi(str_range.substr(pos1+1));
+}
+
+bool FioRange::check(Fio fio)
+{
+    if (firstName == "" && lastName == "" && patronimic == "")
+        return true;
+    else if ((firstName == "" && lastName == "" && patronimic == fio.patronimic)||
+            (firstName == "" && lastName == fio.lastName && patronimic == "")||
+            (firstName == fio.firstName && lastName == "" && patronimic == ""))
+            return true;
+    else if (firstName == fio.firstName && lastName == fio.lastName && patronimic == fio.patronimic)\
+        return true;
+    else return false;
+}
+FioRange::FioRange(std::string firstName,std::string lastName,std::string patronimic)
+{
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->patronimic = patronimic;
 }
